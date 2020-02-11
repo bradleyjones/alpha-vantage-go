3aac822d85fd87798f537b3d8d0458b45816519c
Bradley Jones <jones.bradley@me.com>
2020-02-11 21:15:27 +0000

    Add get api key function

--- client.go
+++ client.go
@@ -1,12 +1,16 @@
 package alphavantage
 
-type AVClient struct {
+type Client struct {
 	apiKey string
 }
 
 // Return a new alpha vantage client
-func NewClient(apiKey string) *AVClient {
-	return &AVClient{
+func NewClient(apiKey string) *Client {
+	return &Client{
 		apiKey: apiKey,
 	}
+}
+
+func (c *Client) GetAPIKey() string {
+	return c.apiKey
 }
